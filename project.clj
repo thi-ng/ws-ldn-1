c634caf3df6c2b8d760171ff141334eec9983ddb
Karsten Schmidt <k@postspectacular.com>
2015-11-02 13:45:18 +0000

    add project.clj

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject thi.ng/ws-ldn-1 "0.1.0-SNAPSHOT"
+  :description   "thi.ng workshop #1"
+  :url           "http://thi.ng"
+  :license       {:name "Apache Software License 2.0"
+                  :url "http://www.apache.org/licenses/LICENSE-2.0"}
+  :dependencies  [[org.clojure/clojure "1.7.0"]
+                  [org.clojure/clojurescript "1.7.122"]
+                  [thi.ng/geom "0.0.881"]])
